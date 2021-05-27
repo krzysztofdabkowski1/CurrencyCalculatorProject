93a02ccbba5ba854b73aa8449ed7d78e3122b625
Krzysztof <krzysztofdabkowski1@gmail.com>
2021-05-27 10:04:20 +0200

    little formatting improvements

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
         CurrencyXMLParser parser = new CurrencyXMLParser();
-        Map<String, Double> rates = parser.parse();
-        CurrencyCalculator calc = new CurrencyCalculator(rates);
+        Map<String, Double> currencyRates = parser.parse();
+        CurrencyCalculator calc = new CurrencyCalculator(currencyRates);
 
         System.out.println(calc.convertFromEUR(56.67, CurrencyEnum.USD));
     }
